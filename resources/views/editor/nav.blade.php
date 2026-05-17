9bf1aaa4674383111b369c1d0b13b011c13e1507
Guenzandra Ocba <zandraflores2003@gmail.com>
2026-05-17 23:55:57 +0800

    -

--- resources/views/editor/nav.blade.php
+++ resources/views/editor/nav.blade.php
@@ -1039,14 +1039,14 @@
         <span class="nav-label">News & Accomplishments</span>
       </a>
 
-      <a href="{{ route('editor.images') }}"
+      <!-- <a href="{{ route('editor.images') }}"
         class="nav-item {{ request()->routeIs('editor.images') ? 'active' : '' }}">
         <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
         </svg>
         <span class="nav-label">Images</span>
-      </a>
+      </a> -->
 
       <a href="{{ route('editor.documents') }}"
         class="nav-item {{ request()->routeIs('editor.documents') ? 'active' : '' }}">
